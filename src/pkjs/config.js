cd65bb49219462b0c937191e7429bc99da458d05
daktak <daktak@domo.ddns.net>
2026-06-13 20:03:56 +1000

    setting

---
+++ src/pkjs/config.js
@@ -0,0 +1,25 @@
+module.exports = [
+{
+  "type": "heading",
+  "defaultValue": "UQM Configuration"
+},
+{
+  "type": "section",
+  "items": [
+  {
+    "type": "heading",
+    "defaultValue": "Watch Settings"
+  },
+  {
+    "type": "toggle",
+    "messageKey": "Hires",
+    "label": "Hires Captains",
+    "defaultValue": true
+  }
+  ]
+},
+{
+  "type": "submit",
+  "defaultValue": "Save Settings"
+}
+];
