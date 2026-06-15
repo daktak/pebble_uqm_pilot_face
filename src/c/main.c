c5e777a8ea9dd8c406ab4be8da0c657da6c2795a
daktak <daktak@domo.ddns.net>
2026-06-15 15:22:17 +1000

    layout

--- src/c/main.c
+++ src/c/main.c
@@ -14,8 +14,9 @@ static Layer *s_border_layer;
 static GFont s_custom_font;
 
 static int s_current_pilot;
-static GRect s_race_rect;
-static GRect s_cap_rect;
+static int s_win_w;
+static int s_img_y;
+static GRect s_img_border;
 
 static const char *RACE_NAMES[RACE_COUNT] = {
   "Spathi", "Androsynth", "Arilou", "Chenjesu", "Chmmr",
@@ -40,25 +41,30 @@ static const uint32_t PILOT_RESOURCES_HIRES[] = {RESOURCE_ID_ELUDER_HIRES, RESOU
 #if defined(PBL_PLATFORM_GABBRO)
 #define RACE_FONT FONT_KEY_GOTHIC_24
 #define CAP_FONT FONT_KEY_GOTHIC_28
+#define RACE_H 26
+#define CAP_H 30
 #elif defined(PBL_PLATFORM_EMERY)
 #define RACE_FONT FONT_KEY_GOTHIC_18
 #define CAP_FONT FONT_KEY_GOTHIC_24
+#define RACE_H 20
+#define CAP_H 26
 #elif defined(PBL_PLATFORM_CHALK)
 #define RACE_FONT FONT_KEY_GOTHIC_14
 #define CAP_FONT FONT_KEY_GOTHIC_18
+#define RACE_H 16
+#define CAP_H 20
 #else
 #define RACE_FONT FONT_KEY_GOTHIC_14
 #define CAP_FONT FONT_KEY_GOTHIC_18
+#define RACE_H 16
+#define CAP_H 20
 #endif
+#define GAP 4
+#define PAD 4
+#define TIME_H 36
 
 int get_current_pilot() {
   return s_current_pilot;
-}
-
-static void set_initial_pilot() {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  s_current_pilot = tick_time->tm_min % RACE_COUNT;
 }
 
 void update_pilot() {
@@ -69,7 +75,15 @@ void update_pilot() {
     gbitmap_destroy(s_pilot_bitmap);
   }
   s_pilot_bitmap = gbitmap_create_with_resource(resources[s_current_pilot]);
+
+  GSize img_size = gbitmap_get_bounds(s_pilot_bitmap).size;
+  int img_x = (s_win_w - img_size.w) / 2;
+
   bitmap_layer_set_bitmap(s_pilot_layer, s_pilot_bitmap);
+  layer_set_frame(bitmap_layer_get_layer(s_pilot_layer), GRect(img_x, s_img_y, img_size.w, img_size.h));
+
+  s_img_border = GRect(img_x - 2, s_img_y - 2, img_size.w + 4, img_size.h + 4);
+  if (s_border_layer) layer_mark_dirty(s_border_layer);
 
   update_race();
   update_captain_text();
@@ -116,81 +130,78 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   }
 }
 
-static void border_update_proc(Layer *layer, GContext *ctx) {
-#if defined(PBL_COLOR)
+static void draw_bezel(GContext *ctx, GRect r) {
   graphics_context_set_stroke_width(ctx, 2);
   graphics_context_set_stroke_color(ctx, GColorWhite);
-  graphics_draw_line(ctx, GPoint(s_race_rect.origin.x, s_race_rect.origin.y), GPoint(s_race_rect.origin.x + s_race_rect.size.w - 1, s_race_rect.origin.y));
-  graphics_draw_line(ctx, GPoint(s_race_rect.origin.x, s_race_rect.origin.y), GPoint(s_race_rect.origin.x, s_race_rect.origin.y + s_race_rect.size.h - 1));
+  graphics_draw_line(ctx, GPoint(r.origin.x, r.origin.y), GPoint(r.origin.x + r.size.w - 1, r.origin.y));
+  graphics_draw_line(ctx, GPoint(r.origin.x, r.origin.y), GPoint(r.origin.x, r.origin.y + r.size.h - 1));
   graphics_context_set_stroke_color(ctx, GColorDarkGray);
-  graphics_draw_line(ctx, GPoint(s_race_rect.origin.x + s_race_rect.size.w - 1, s_race_rect.origin.y), GPoint(s_race_rect.origin.x + s_race_rect.size.w - 1, s_race_rect.origin.y + s_race_rect.size.h - 1));
-  graphics_draw_line(ctx, GPoint(s_race_rect.origin.x, s_race_rect.origin.y + s_race_rect.size.h - 1), GPoint(s_race_rect.origin.x + s_race_rect.size.w - 1, s_race_rect.origin.y + s_race_rect.size.h - 1));
+  graphics_draw_line(ctx, GPoint(r.origin.x + r.size.w - 1, r.origin.y), GPoint(r.origin.x + r.size.w - 1, r.origin.y + r.size.h - 1));
+  graphics_draw_line(ctx, GPoint(r.origin.x, r.origin.y + r.size.h - 1), GPoint(r.origin.x + r.size.w - 1, r.origin.y + r.size.h - 1));
+}
 
-  graphics_context_set_stroke_color(ctx, GColorWhite);
-  graphics_draw_line(ctx, GPoint(s_cap_rect.origin.x, s_cap_rect.origin.y), GPoint(s_cap_rect.origin.x + s_cap_rect.size.w - 1, s_cap_rect.origin.y));
-  graphics_draw_line(ctx, GPoint(s_cap_rect.origin.x, s_cap_rect.origin.y), GPoint(s_cap_rect.origin.x, s_cap_rect.origin.y + s_cap_rect.size.h - 1));
-  graphics_context_set_stroke_color(ctx, GColorDarkGray);
-  graphics_draw_line(ctx, GPoint(s_cap_rect.origin.x + s_cap_rect.size.w - 1, s_cap_rect.origin.y), GPoint(s_cap_rect.origin.x + s_cap_rect.size.w - 1, s_cap_rect.origin.y + s_cap_rect.size.h - 1));
-  graphics_draw_line(ctx, GPoint(s_cap_rect.origin.x, s_cap_rect.origin.y + s_cap_rect.size.h - 1), GPoint(s_cap_rect.origin.x + s_cap_rect.size.w - 1, s_cap_rect.origin.y + s_cap_rect.size.h - 1));
+static void border_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+#if defined(PBL_COLOR)
+  draw_bezel(ctx, bounds);
+  if (s_img_border.size.w > 0 && s_img_border.size.h > 0) {
+    draw_bezel(ctx, s_img_border);
+  }
 #else
   graphics_context_set_stroke_width(ctx, 1);
   graphics_context_set_stroke_color(ctx, GColorWhite);
-  graphics_draw_rect(ctx, s_race_rect);
-  graphics_draw_rect(ctx, s_cap_rect);
+  graphics_draw_rect(ctx, bounds);
+  if (s_img_border.size.w > 0 && s_img_border.size.h > 0) {
+    graphics_draw_rect(ctx, s_img_border);
+  }
 #endif
 }
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  int w = bounds.size.w;
+  s_win_w = bounds.size.w;
   int h = bounds.size.h;
 
-  int pilot_h = h * 55 / 100;
-  int time_h = 36;
-  int time_y = h - time_h - 4;
+  int time_y = h - PAD - TIME_H;
+  int cap_y = time_y - GAP - CAP_H;
+  int race_y = PAD;
+  s_img_y = race_y + RACE_H + GAP;
 
-  int info_y = pilot_h + 4;
-  int info_h = time_y - info_y - 4;
+  s_custom_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UQM_24));
 
-  int race_h = info_h / 2;
-  int cap_h = info_h - race_h;
-
-  s_race_rect = GRect(2, info_y + 1, w - 4, race_h - 2);
-  s_cap_rect = GRect(2, info_y + race_h + 1, w - 4, cap_h - 2);
-
-  s_pilot_layer = bitmap_layer_create(GRect(0, 0, w, pilot_h));
+  s_pilot_layer = bitmap_layer_create(GRect(0, s_img_y, s_win_w, 100));
   bitmap_layer_set_compositing_mode(s_pilot_layer, GCompOpSet);
-  bitmap_layer_set_alignment(s_pilot_layer, GAlignCenter);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_pilot_layer));
 
-  s_race_layer = text_layer_create(GRect(s_race_rect.origin.x + 3, s_race_rect.origin.y + 3, s_race_rect.size.w - 6, s_race_rect.size.h - 6));
+  s_race_layer = text_layer_create(GRect(PAD, race_y, s_win_w - PAD * 2, RACE_H));
   text_layer_set_background_color(s_race_layer, GColorClear);
   text_layer_set_text_color(s_race_layer, GColorWhite);
   text_layer_set_font(s_race_layer, fonts_get_system_font(RACE_FONT));
   text_layer_set_text_alignment(s_race_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_race_layer));
 
-  s_cap_layer = text_layer_create(GRect(s_cap_rect.origin.x + 3, s_cap_rect.origin.y + 3, s_cap_rect.size.w - 6, s_cap_rect.size.h - 6));
+  s_cap_layer = text_layer_create(GRect(PAD, cap_y, s_win_w - PAD * 2, CAP_H));
   text_layer_set_background_color(s_cap_layer, GColorClear);
   text_layer_set_text_color(s_cap_layer, GColorWhite);
   text_layer_set_font(s_cap_layer, fonts_get_system_font(CAP_FONT));
   text_layer_set_text_alignment(s_cap_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_cap_layer));
 
-  s_border_layer = layer_create(bounds);
-  layer_set_update_proc(s_border_layer, border_update_proc);
-  layer_add_child(window_layer, s_border_layer);
-
-  s_custom_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UQM_24));
-  s_time_layer = text_layer_create(GRect(0, time_y, w, time_h));
+  s_time_layer = text_layer_create(GRect(PAD, time_y, s_win_w - PAD * 2, TIME_H));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_font(s_time_layer, s_custom_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
-  set_initial_pilot();
+  s_border_layer = layer_create(bounds);
+  layer_set_update_proc(s_border_layer, border_update_proc);
+  layer_add_child(window_layer, s_border_layer);
+
+  time_t now = time(NULL);
+  struct tm *tick_time = localtime(&now);
+  s_current_pilot = tick_time->tm_min % RACE_COUNT;
   update_pilot();
   update_time();
 }
